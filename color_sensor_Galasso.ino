2d32cf8a231363878b328a5062b82bcda86ed921
Zigurater <36968618+Zigurater@users.noreply.github.com>
2018-03-02 14:01:08 -0600

    Add files via upload

--- color_sensor_Galasso.ino
+++ color_sensor_Galasso.ino
@@ -107,8 +107,9 @@ void loop() {
 
 void move_servo(){
   if (planettype == 3) {
+    pos = 180;
     delay (1000);
-      for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      for (pos = 180; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       myservo.write(pos);       // tell servo to go to position in variable 'pos'
       delay(10);             // waits 15ms for the servo to reach the position
